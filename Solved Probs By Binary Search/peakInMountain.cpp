be90708eb2ae7aa822e99df1c30fea76cc1a19da
Utkarsh-976 <utkarshtripathiofficialll@gmail.com>
2026-05-11 22:56:23 +0530

    Update peakInMountain.cpp

--- Solved Probs By Binary Search/peakInMountain.cpp
+++ Solved Probs By Binary Search/peakInMountain.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
   
-    int find_pivot(vector<int> v) {
+    int find_pivot(vector <int> v) {
 	int s = 0, e = v.size() - 1;
 	int mid=(s + e) / 2;
 	while (s < e)
